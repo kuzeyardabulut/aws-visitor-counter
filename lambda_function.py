a42749d91fbb4e4e1ef60be7142a9385322ebc1c
kuzeyardabulut <54737933+kuzeyardabulut@users.noreply.github.com>
2022-03-24 22:52:09 +0300

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,24 @@
+import json
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+ddbTableName = 'VisitorCount'
+table = dynamodb.Table(ddbTableName)
+
+def lambda_handler(event, context):
+
+    response = table.get_item(Key= {'id' : 'count'} )
+    count = response["Item"]["visitor_count"]
+
+    # increment string version of visit count
+    new_count = str(int(count)+1)
+    response = table.update_item(
+        Key={'id': 'count'},
+        UpdateExpression='set visitor_count = :c',
+        ExpressionAttributeValues={':c': new_count},
+        ReturnValues='UPDATED_NEW'
+        )
+
+    return {'Count':new_count}
+#end lambda_handler
